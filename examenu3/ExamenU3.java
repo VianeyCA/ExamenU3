b46e95d15a868d7f6fbab64e4a40abe642829bdd
Vianey Ambrosio <71795759+VianeyCA@users.noreply.github.com>
2020-11-22 18:53:45 -0600

    Update ExamenU3.java

--- examenu3/ExamenU3.java
+++ examenu3/ExamenU3.java
@@ -7,7 +7,7 @@ public class ExamenU3 {
      public static void main(String[] args) {
          
          Scanner leer = new Scanner(System.in);
-         ColaDinamica caso = new ColaDinamica();
+         ColaDinamica envioNodo = new ColaDinamica();
          int opc;
          int i = 0;
          int j=0;
@@ -24,36 +24,37 @@ public class ExamenU3 {
              switch (opc) {
                  case 1:
                      boolean VIP = VIP();
-                     if (VIP) {
+                         if(i<=0){
                          i=(int)(Math.random()*10+1);
-                         i++;
-                         System.out.println("Cliente VIP: "+ "VIP"+i);
-                         caso.Cliente("VIP", VIP, i);
-                         } else{
-                         i=(int)(Math.random()*10+1);
-                         i++;
-                         System.out.println("Cliente Común: " + i + "Común"+i);
-                         caso.Cliente("Comun", VIP, i);
+                         }else{
+                             i++;
+                         }
+                         if (VIP) {
+                         System.out.println("Cliente VIP:"+i+ "VIP"+i);
+                         envioNodo.Cliente("VIP", VIP, i);
+                         } else{                         
+                         System.out.println("Cliente Común:"+i+ "Común"+i);
+                         envioNodo.Cliente("Comun", VIP, i);
                      }                         
                      System.out.println("");
                      break;
                      
                  case 2:
-                     if (!caso.Vacio()) {
-                         caso.Pasa();
+                     if (!envioNodo.Vacio()) {
+                         envioNodo.Pasa();
                      } else {
                          System.out.println("No existe ningún cliente en las colas");
                      }
                      System.out.println("");
                      break;
                  case 3:
-                     caso.Mostrar();
+                     envioNodo.Mostrar();
                      System.out.println("");
                      break;
                  case 4:
-                     if (!caso.Vacio()) {
+                     if (!envioNodo.Vacio()) {
                          int n = (int) (Math.random() * 3);
-                         caso.Ventana(n);
+                         envioNodo.Ventana(n);
                      } else {
                          System.out.println("Las colas estan vacias");
                      }
@@ -73,6 +74,4 @@ public class ExamenU3 {
              num= (int) (Math.random() * 2);         
          return num == 0;
       }
-     
-    
 }
